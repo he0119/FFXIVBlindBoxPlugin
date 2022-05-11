834b93dd2b5d169e7e5272e0aef70e515e38c7b6
hemengyang <hmy0119@gmail.com>
2022-05-11 17:30:20 +0800

    ci: 修复生成脚本 [skip ci]

--- .github/scripts/Make-Repo.ps1
+++ .github/scripts/Make-Repo.ps1
@@ -1,55 +1,27 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
 $output = New-Object Collections.Generic.List[object]
-$notInclude = "MapLinker";
-
-$counts = Get-Content "downloadcounts.json" | ConvertFrom-Json
-
-$dlTemplateInstall = "https://github.com/he0119/DalamudPlugins/raw/main/plugins/{0}/latest.zip"
-$dlTemplateUpdate = "https://github.com/he0119/DalamudPlugins/raw/main/plugins/{0}/latest.zip"
-
-$thisPath = Get-Location
-
-$table = ""
 
 $content = Get-Content "build/BlindBoxPlugin.json" | ConvertFrom-Json
 
-if ($notInclude.Contains($content.InternalName)) {
-    $content | add-member -Name "IsHide" -value "True" -MemberType NoteProperty
-}
-else
-{
-    $content | add-member -Name "IsHide" -value "False" -MemberType NoteProperty
+$dlTemplateInstall = "https://github.com/he0119/FFXIVBlindBoxPlugin/releases/download/v{0}/latest.zip"
+$dlTemplateUpdate = "https://github.com/he0119/FFXIVBlindBoxPlugin/releases/download/v{0}/latest.zip"
 
-    $newDesc = $content.Description -replace "\n", "<br>"
-    $table = $table + "| " + $content.Author + " | " + $content.Name + " | " + $newDesc + " |`n"
-}
+$content | add-member -Name "IsHide" -value "False" -MemberType NoteProperty
 
-$testingPath = Join-Path $thisPath -ChildPath "testing" | Join-Path -ChildPath $content.InternalName | Join-Path -ChildPath $_.Name
-if ($testingPath | Test-Path)
-{
-    $testingContent = Get-Content $testingPath | ConvertFrom-Json
-    $content | add-member -Name "TestingAssemblyVersion" -value $testingContent.AssemblyVersion -MemberType NoteProperty
-}
 $content | add-member -Name "IsTestingExclusive" -value "False" -MemberType NoteProperty
 
-$dlCount = $counts | Select-Object -ExpandProperty $content.InternalName | Select-Object -ExpandProperty "count"
-if ($dlCount -eq $null){
-    $dlCount = 0;
-}
+$dlCount = 0;
 $content | add-member -Name "DownloadCount" $dlCount -MemberType NoteProperty
 
-$internalName = $content.InternalName
-$updateDate = git log -1 --pretty="format:%ct" plugins/$internalName/latest.zip
+$updateDate = Get-Date -UFormat %s
 $content | add-member -Name "LastUpdate" $updateDate -MemberType NoteProperty
 
-$installLink = $dlTemplateInstall -f $internalName, "False"
-$content | add-member -Name "DownloadLinkInstall" $installLink -MemberType NoteProperty
-
-$installLink = $dlTemplateInstall -f $internalName, "True"
+$assemblyVersion = $content.AssemblyVersion
+$installLink = $dlTemplateInstall -f $assemblyVersion
 $content | add-member -Name "DownloadLinkTesting" $installLink -MemberType NoteProperty
 
-$updateLink = $dlTemplateUpdate -f $internalName, "False"
+$updateLink = $dlTemplateUpdate -f $assemblyVersion
 $content | add-member -Name "DownloadLinkUpdate" $updateLink -MemberType NoteProperty
 
 $output.Add($content)
