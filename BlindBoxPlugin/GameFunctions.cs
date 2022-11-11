b37ae8b3e53b23459021a831c42a567d72181150
hemengyang <hmy0119@gmail.com>
2022-11-11 13:33:27 +0800

    适配 6.1 版本

--- BlindBoxPlugin/GameFunctions.cs
+++ BlindBoxPlugin/GameFunctions.cs
@@ -8,28 +8,33 @@ namespace BlindBoxPlugin
     {
         private static bool initialized = false;
 
-        private delegate byte HasItemActionUnlockedDelegate(IntPtr mem);
-        private static HasItemActionUnlockedDelegate _hasItemActionUnlocked = null!;
         private delegate byte HasCardDelegate(IntPtr localPlayer, ushort cardId);
-        private static HasCardDelegate _hasCard = null!;
+        private unsafe delegate byte HasItemActionUnlockedDelegate(long mem, long a2, long* a3);
+        private delegate long ItemToUlongDelegate(uint id);
+
         private static IntPtr _cardStaticAddr;
+        private static HasCardDelegate _hasCard = null!;
+        private static HasItemActionUnlockedDelegate _hasItemActionUnlocked = null!;
+        private static ItemToUlongDelegate _itemToUlong = null!;
 
         public static void Initialize()
         {
             if (initialized) return;
             initialized = true;
 
-            var hasIaUnlockedPtr = BlindBox.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 75 A9");
+            var hasIaUnlockedPtr = BlindBox.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 75 A6 32 C0");
             var hasCardPtr = BlindBox.SigScanner.ScanText("40 53 48 83 EC 20 48 8B D9 66 85 D2 74");
+            var itemToUlongPtr = BlindBox.SigScanner.ScanText("E8 ?? ?? ?? ?? 48 85 C0 74 33 83 7F 04 00");
             _cardStaticAddr = BlindBox.SigScanner.GetStaticAddressFromSig("41 0F B7 17 48 8D 0D");
 
-            if (hasIaUnlockedPtr == IntPtr.Zero || hasCardPtr == IntPtr.Zero || _cardStaticAddr == IntPtr.Zero)
+            if (hasIaUnlockedPtr == IntPtr.Zero || hasCardPtr == IntPtr.Zero || _cardStaticAddr == IntPtr.Zero || itemToUlongPtr == IntPtr.Zero)
             {
                 throw new ApplicationException("Could not get pointers for game functions");
             }
 
             _hasItemActionUnlocked = Marshal.GetDelegateForFunctionPointer<HasItemActionUnlockedDelegate>(hasIaUnlockedPtr);
             _hasCard = Marshal.GetDelegateForFunctionPointer<HasCardDelegate>(hasCardPtr);
+            _itemToUlong = Marshal.GetDelegateForFunctionPointer<ItemToUlongDelegate>(itemToUlongPtr);
         }
 
         // https://git.annaclemens.io/ascclemens/GoodMemory/src/branch/main/GoodMemory/GameFunctions.cs
@@ -64,6 +69,12 @@ namespace BlindBoxPlugin
                 return false;
             }
 
+            long itemId = _itemToUlong(item.RowId);
+            if (itemId == 0L)
+            {
+                return false;
+            }
+
             var type = (ActionType)itemAction.Type;
 
             var mem = Marshal.AllocHGlobal(256);
@@ -73,8 +84,7 @@ namespace BlindBoxPlugin
             {
                 *(uint*)(mem + 112) = item.AdditionalData;
             }
-
-            var ret = _hasItemActionUnlocked(mem) == 1;
+            var ret = _hasItemActionUnlocked(itemId, 0L, (long*)mem) == 1;
 
             Marshal.FreeHGlobal(mem);
 
