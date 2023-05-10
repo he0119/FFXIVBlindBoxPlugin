70071cf6ec946f794b245b599b47efa119f6647a
uy_sun <hmy0119@gmail.com>
2023-05-10 09:30:01 +0800

    移除 XivCommon

--- BlindBoxPlugin/GameFunctions.cs
+++ BlindBoxPlugin/GameFunctions.cs
@@ -1,15 +1,19 @@
 // 物品获取信息
-// https://github.com/VergilGao/GoodMemoryCN/blob/master/GoodMemory/GameFunctions.cs
+
 
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Component.Exd;
 using Lumina.Excel.GeneratedSheets;
-using System;
 
 namespace BlindBoxPlugin
 {
     public unsafe class GameFunctions
     {
+        /// <summary>
+        /// https://github.com/Critical-Impact/CriticalCommonLib/blob/d00ed298fd08795c66ed93975144fdec27e1f544/Services/GameInterface.cs#L74
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public static bool HasAcquired(Item item)
         {
             var action = item.ItemAction.Value;
